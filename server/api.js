3dd2f01b0de1409541c821049662f42a4e6248b1
Kurama <man.schibli@gmail.com>
2023-05-08 15:47:11 +0200

    Co-authored-by: Black Viper <DarkDesignX@users.noreply.github.com>

---
+++ server/api.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+//Endpoint for user login
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  const isValed = true;
+
+  if (isValed) {
+    res.json({
+      succes: true,
+      message: "Login successfully. Welcome!",
+    });
+  } else {
+    res.json({
+      succes: false,
+      message: "Login Failed",
+    });
+  }
+});
+
+// Endpoint for user registration
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+
+  const isTaken = false;
+
+  if (isTaken) {
+    res.json({
+      succes: false,
+      message: "Username already taken. Please provide another username",
+    });
+  } else {
+    res.json({
+      success: true,
+      message: "Registration successfull. Welcome!",
+    });
+  }
+});

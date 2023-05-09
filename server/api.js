8dab6ce3b483716d61fea770ff2d85dc85755dba
Denis <denis.basler@gmx.ch>
2023-05-09 13:43:02 +0200

    template for project. Source: https://github.com/SwitzerChees/321-project-starter

--- server/api.js
+++ server/api.js
@@ -1,44 +1,10 @@
-const express = require("express");
-const app = express();
+const initializeAPI = (app) => {
+  // default REST api endpoint
+  app.get("/api/hello", hello);
+};
 
-app.use(express.json());
+const hello = (req, res) => {
+  res.send("Hello World!");
+};
 
-//Endpoint for user login
-app.post("/login", (req, res) => {
-  const { username, password } = req.body;
-
-
-  //write on diffrent approach with http status code
-  const isValed = true;
-
-  if (isValed) {
-    res.json({
-      succes: true,
-      message: "Login successfully. Welcome!",
-    });
-  } else {
-    res.json({
-      succes: false,
-      message: "Login Failed",
-    });
-  }
-});
-
-// Endpoint for user registration
-app.post("/register", (req, res) => {
-  const { username, password } = req.body;
-
-  const isTaken = false;
-
-  if (isTaken) {
-    res.json({
-      succes: false,
-      message: "Username already taken. Please provide another username",
-    });
-  } else {
-    res.json({
-      success: true,
-      message: "Registration successfull. Welcome!",
-    });
-  }
-});
+module.exports = { initializeAPI };
